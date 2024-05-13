4f60039bc172353a280d01b74e80d7e6d6296f53
dibek <dibekpoudyal@gmail.com>
2024-05-13 11:44:04 +0545

    insert, not null, default

--- commands/tables.sql
+++ commands/tables.sql
@@ -1,9 +1,9 @@
 -- creating a table
 create table if not exists :table_name
 (
-    username   varchar(15),
-    content    varchar(140),
-    favourites integer
+    username   varchar(15) not null,
+    content    varchar(140) not null,
+    favourites integer not null default 0
 );
 
 -- show tables
@@ -14,4 +14,16 @@ create table if not exists :table_name
 \d+ :table_name
 
 -- dropping table
-drop table if exists :table_name
+drop table if exists :table_name;
+
+-- insert data into table
+insert into dogs (name, age)
+values ('muffin', 11);
+
+-- insert multiple data into table
+insert into dogs (name, age)
+values ('goldie', 3),
+       ('punte', 5);
+
+-- get all data from table
+select * from dogs;
