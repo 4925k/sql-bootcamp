1f6c19af2b4169a07361b8223c46890d2e013da8
dibek <dibekpoudyal@gmail.com>
2024-05-19 14:12:04 +0545

    string functions continued

--- commands/string_functions.sql
+++ commands/string_functions.sql
@@ -1,27 +1,40 @@
--- concat
+-- CONCAT
 SELECT concat(author_fname, ' ', author_lname) as full_name from books;
 
 -- concat with separator value
 SELECT concat_ws('-', author_fname, author_lname) as full_name from books;
 
 
--- substring
+-- SUBSTRING
 select substring('hello world', 1, 5);      -- hello
 select substring('hello world', 6);         -- world
-select substring('hello world', -5);        -- world
-
 select substring(title, 1, 15) as title from books;
-
 -- getting short title
 select concat(
                substring(title, 1, 10),
                '...'
             ) as short_title
         from books;
-
 -- getting author initials
-select concat(
-       substring(author_fname, 1, 1),
-       substring(author_lname, 1, 1)
-       ) as initials
-from books;
+select
+    concat(
+            substring(author_fname, 1, 1),
+            substring(author_lname, 1, 1)
+    ) as initials
+from
+    books;
+
+-- REPLACE
+select replace('hello-world', 'world', 'dbk');
+
+-- REVERSE
+select reverse('hello-world');
+
+-- CHAR LENGTH
+select char_length('hello-world');
+
+-- UPPER && LOWER
+select upper('postgres');
+select lower('POSTGRES');
+
+-- https://www.postgresql.org/docs/9.1/functions-string.html -- OTHER STRING FUNCTIONS
