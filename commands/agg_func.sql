854155190f9edf911564fa5a54dc883b6d45fac3
dibek <dibekpoudyal@gmail.com>
2024-05-25 13:26:04 +0545

    sum, avg, max, min, groupby

--- commands/agg_func.sql
+++ commands/agg_func.sql
@@ -7,9 +7,27 @@ select count(distinct author_fname) from books;
 select author_lname from books group by author_lname;
 select author_lname, COUNT(*) from books group by author_lname;
 
--- max and min
+-- GROUP BY Multiple Columns
+select author_fname, author_lname, Count(*) from books group by author_fname, author_lname;
+select concat(author_fname, ' ', author_lname) as author, count(*) from books group by author;
+
+-- MAX & MIN
 select MAX(pages) from books;
 select MIN(pages) from books;
+
+-- MAX & MIN + GROUP BY
+select author_lname, min(released_year) from books group by author_lname;
+select author_lname, max(released_year) from books group by author_lname;
+select
+    concat(author_fname, ' ', author_lname) as author,
+    count(*) as total_books,
+    min(released_year) as first_release,
+    max(released_year) as recent_release
+from
+    books
+group by
+    author_fname, author_lname;
+
 
 -- sub queries
 select
@@ -19,4 +37,13 @@ from
 where
     pages = (
         select max(pages) from books
-        );
+    );
+
+-- SUM
+select sum(pages) from books;
+select author_lname, sum(pages) from books group by author_lname;
+
+-- AVG
+select avg(released_year) from books;
+select avg(pages) from books;
+select released_year, avg(stock_quantity) from books group by released_year;
