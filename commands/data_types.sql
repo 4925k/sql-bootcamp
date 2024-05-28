ef16eb5fa810db3b520597b3b7d8379a5c0912c3
dibek <dibekpoudyal@gmail.com>
2024-05-28 15:08:44 +0545

    date, time, timestamp

--- commands/data_types.sql
+++ commands/data_types.sql
@@ -9,3 +9,20 @@ create table family (
     father text,
     children smallint
 )
+
+-- DATE TIME
+create table people (
+    name text,
+    birthdate DATE,
+    birth_time time,
+    birth_datetime timestamp
+);
+
+insert into
+    people (name, birthdate, birth_time, birth_datetime)
+VALUES (
+        'Dameon',
+        '2000-01-01',
+        '10:20:30',
+        '2004-01-01 10:20:30'
+       );
