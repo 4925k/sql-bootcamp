f7fac6a78bedf4e09b6e9be8e8d637d9e6182e4a
dibek <dibekpoudyal@gmail.com>
2024-06-01 14:23:24 +0545

    date formatting exercises

--- commands/data_types.sql
+++ commands/data_types.sql
@@ -31,4 +31,18 @@ VALUES (
 select current_date;
 select current_time;
 select now();
-select date_part('year', now();
+select date_part('year', now());
+
+-- EXERCISES
+select current_time;            -- current time
+select current_date;            -- current date
+select to_char(now(), 'D');     -- current day -> number
+select to_char(now(), 'DY');    -- current day -> name
+select to_char(now(), 'MM/DD/YYYY');                -- date formatting
+select to_char(now(), 'FMMonth DDth at HH:MM');     -- date formatting
+
+create table tweets (
+    content varchar(180),
+    username varchar(15),
+    created_at timestamp DEFAULT current_timestamp
+);
