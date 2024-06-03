0abaabc00d370d7435e63ba4b05151db30e44730
dibek <dibekpoudyal@gmail.com>
2024-06-03 11:44:28 +0545

    comparision operator continue

--- commands/comparision.sql
+++ commands/comparision.sql
@@ -6,3 +6,14 @@ select * from books where title not ilike '% %'; -- no spaces in title
 
 -- GREATER THAN
 select * from books where stock_quantity > 100;
+select * from books where stock_quantity >= 100;
+
+
+-- LESS THAN OR EQUAL TO
+select * from books where stock_quantity <= 20;
+
+-- LOGICAL AND
+select * from books where stock_quantity <= 20 AND released_year >= 2001;
+
+-- LOGICAL OR
+select * from books where author_fname = 'David' OR author_fname = 'Dave';
