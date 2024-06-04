e75a9110402f59aac0096f6fe4f69cc5983b698f
dibek <dibekpoudyal@gmail.com>
2024-06-04 13:39:22 +0545

    between and in

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 up:
 	docker compose up -d
-	sleep 2
+	sleep 1
 	PGPASSWORD='example' psql -U postgres -h localhost -p 5433
 
 down:
 	docker compose down
+
+psql:
+	psql -U postgres -h localhost -p 5433
--- commands/comparision.sql
+++ commands/comparision.sql
@@ -17,3 +17,10 @@ select * from books where stock_quantity <= 20 AND released_year >= 2001;
 
 -- LOGICAL OR
 select * from books where author_fname = 'David' OR author_fname = 'Dave';
+
+-- BETWEEN
+select * from books where released_year between 2001 AND 2020;
+
+-- IN
+select * from books where author_fname IN ('Dave', 'Neil');
+select * from books where author_fname NOT IN ('Dave', 'Neil');
