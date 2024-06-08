8c2551640dbc80047a70c2b1973771773cbabf12
dibek <dibekpoudyal@gmail.com>
2024-06-08 14:51:03 +0545

    alter column and constraint

--- commands/alter.sql
+++ commands/alter.sql
@@ -8,5 +8,9 @@ alter table babies drop column gender;
 alter table babies_2 rename to children;
 
 -- rename column
-
 alter table children rename gender to sex;
+
+-- alter column
+alter table babies alter column name type char(50);
+alter table babies add constraint age_check check (age > 0);
+alter table babies drop constraint age_check;
