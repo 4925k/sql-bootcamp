9c5383bcd44a8b441ee01dd1b278bc9117a5efae
dibek <dibekpoudyal@gmail.com>
2024-06-22 13:20:31 +0545

    windows functions + lead and lag

--- commands/window_func.sql
+++ commands/window_func.sql
@@ -57,6 +57,40 @@ select
     sum(salary) over( order by salary) as grand_running_total
 from employees;
 
+-- rank of salary among department and overall
+select
+    employees.emp_no,
+    employees.department,
+    employees.salary,
+    rank() over(partition by department order by salary desc) as dept_rank, -- give rank to each row within department
+    rank() over(order by salary desc) as overall_rank, -- can skip numbers if multiple rows have same number
+    row_number() over (order by salary desc) as row_number, -- give row number to each row
+    dense_rank() over (order by salary desc) as overall_dense_rank -- rank but no skipping
+from employees order by row_number;
+
+-- ntile() divide partition into n groups
 select
     emp_no,
+    department,
+    salary,
+    ntile(4) over(partition by employees.department order by salary desc) as dept_quartile,
+    ntile(4) over(order by salary desc) as quartile
+from employees order by department;
 
+-- first value -> gives the first value of the window
+select
+    emp_no,
+    department,
+    salary,
+    first_value(emp_no) over(order by salary desc) as highest_paid,
+    first_value(emp_no) over(partition by department order by salary desc) as highest_paid_dept
+from employees;
+
+-- lead and lag -> access values from other rows in the window
+select
+    emp_no,
+    department,
+    salary,
+    lag(salary) over(order by salary) as previous_salary,
+    lead(salary) over(order by salary) as next_salary
+from employees order by salary desc ;
