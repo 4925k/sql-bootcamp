a00553698389f9b435332a8d7d436c643bbb975c
dibek <dibekpoudyal@gmail.com>
2024-06-27 14:02:49 +0545

    ig clone exercises

--- exercises/ig.sql
+++ exercises/ig.sql
@@ -11,3 +11,19 @@ from
     users
 group by day
 order by count desc;
+
+-- inactive users -> people who have never posted a photo
+select username from users
+left join photos on users.id = photos.user_id
+where photos.id is null;
+
+-- who got the most likes\
+select distinct username from users
+join photos on users.id = photos.user_id
+where photos.id = (
+    select photos.id from photos
+    join likes on photos.id = likes.photo_id
+    group by photos.id
+    order by count(*) desc
+    limit 1
+);
