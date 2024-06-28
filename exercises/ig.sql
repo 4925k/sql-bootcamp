feb02fae0becb8ab915f056c1f5f4322bedf5ffa
dibek <dibekpoudyal@gmail.com>
2024-06-28 16:16:00 +0545

    ig clone exercises

--- exercises/ig.sql
+++ exercises/ig.sql
@@ -27,3 +27,20 @@ where photos.id = (
     order by count(*) desc
     limit 1
 );
+
+
+-- avg number of likes per photo
+select  (select count(*) from photos) / (select count(*) from users ) as avg_posts;
+
+-- top 5 used hashtags
+select tags.tag_name, count(*) as used from photo_tags
+join tags on photo_tags.tag_id = tags.id
+group by tags.id
+order by used desc
+limit 5;
+
+-- user who has liked all the photos
+select username, count(*) as liked_photos from users
+inner join likes on users.id = likes.user_id
+group by users.id
+having count(*) = (select count(*) from photos);
